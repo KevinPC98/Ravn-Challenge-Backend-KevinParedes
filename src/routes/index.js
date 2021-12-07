4937601b225e0e8458539e368e2abce50d57e6aa
KevinPC98 <kevin.paredescaman20@gmail.com>
2021-12-07 15:41:22 -0500

    Routes modificado nuevas rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getAuthors, getAuthorById } = require("../controllers/index");
+const { getAuthors, getAuthorById, getTopAuthors, getTopAuthorsByCount } = require("../controllers/index");
 
 router.get("/autor", getAuthors);
 router.get("/autor/:id", getAuthorById);
+router.get("/top/autor", getTopAuthors);
+router.get("/top/autor/:count", getTopAuthorsByCount);
 
 module.exports = router;
