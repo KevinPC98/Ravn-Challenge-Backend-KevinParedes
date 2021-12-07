bb8a6721d7687676e699218093af2d6fd2693dc7
KevinPC98 <kevin.paredescaman20@gmail.com>
2021-12-07 15:43:08 -0500

    Otra forma de leer el puerto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.set("port",3000);
+
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 //routes
 app.use(require("./routes/index"));
 
-app.listen(3000);
-console.log("SERVER RUNNING");
+
+app.listen(app.get('port'), () => {
+    console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
+});
