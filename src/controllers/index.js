acc251d5a2c1cb5c5914ec99237079d739e41cf5
KevinPC98 <kevin.paredescaman20@gmail.com>
2021-12-07 15:40:26 -0500

    Controllers modificado con nuevas consultas a la BD

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -10,16 +10,42 @@ const pool = new Pool({
 
 const getAuthors = async (req, res) => {
     const response = await pool.query('SELECT * FROM authors');
-    res.status(200).json(response.rows);
+    res.json(response.rows);
 };
 
 const getAuthorById = async (req, res) => {
     const id = parseInt(req.params.id);
     const response = await pool.query('SELECT * FROM authors WHERE id = $1', [id]);   
+    res.res.json(response.rows);
+};
+
+//Top 10 de autores por ventas
+const getTopAuthors = async (req, res) => {
+    const response = await pool.query(`SELECT authors.name, SUM(VENTAS) AS INGRESO_VENTA FROM
+    (SELECT books.author_id, quantity*item_price AS VENTAS FROM books INNER JOIN sale_items ON books.id = sale_items.book_id)
+    AS T1 INNER JOIN authors ON T1.author_id = authors.id GROUP BY authors.name ORDER BY INGRESO_VENTA DESC LIMIT 10;`);
     res.json(response.rows);
+};
+
+//top "count" autores
+const getTopAuthorsByCount = async (req, res) => {
+    const count = parseInt(req.params.count);
+    if (count>=0) {
+        const response = await pool.query(`SELECT authors.name, SUM(VENTAS) AS INGRESO_VENTA FROM
+        (SELECT books.author_id, quantity*item_price AS VENTAS FROM books INNER JOIN sale_items ON books.id = sale_items.book_id)
+        AS T1 INNER JOIN authors ON T1.author_id = authors.id GROUP BY authors.name ORDER BY INGRESO_VENTA DESC LIMIT $1;`,[count]);
+        res.json(response.rows);
+    } else {
+        res.status(404).json({
+            status: 'Error',
+            message: "el parametro 'count' no debe ser negativo"
+        });
+    }
 };
 
 module.exports = {
     getAuthors,
-    getAuthorById
+    getAuthorById,
+    getTopAuthors,
+    getTopAuthorsByCount
 };
