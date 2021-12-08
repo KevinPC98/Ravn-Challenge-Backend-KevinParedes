a401e7bebda0f15d29c6a53216fe3f7ef98015e6
KevinPC98 <kevin.paredescaman20@gmail.com>
2021-12-08 18:21:29 -0500

    Validando conexión a la BD

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -9,33 +9,48 @@ const pool = new Pool({
 });
 
 const getAuthors = async (req, res) => {
-    const response = await pool.query('SELECT * FROM authors');
-    res.json(response.rows);
+    try{
+        const response = await pool.query('SELECT * FROM authors');
+        res.json(response.rows);
+    }catch(error){
+        res.json({error});
+    }
 };
 
 const getAuthorById = async (req, res) => {
-    const id = parseInt(req.params.id);
-    const response = await pool.query('SELECT * FROM authors WHERE id = $1', [id]);   
-    res.res.json(response.rows);
+    try{
+        const id = parseInt(req.params.id);
+        const response = await pool.query('SELECT * FROM authors WHERE id = $1', [id]);  
+        res.json(response.rows);
+    }catch(error){
+        console.log(error);
+        res.json({error});
+    }
 };
 
 //Top 10 de autores por ventas
 const getTopAuthors = async (req, res) => {
-    const response = await pool.query(`SELECT authors.name, SUM(VENTAS) AS INGRESO_VENTA FROM
-    (SELECT books.author_id, quantity*item_price AS VENTAS FROM books INNER JOIN sale_items ON books.id = sale_items.book_id)
-    AS T1 INNER JOIN authors ON T1.author_id = authors.id GROUP BY authors.name ORDER BY INGRESO_VENTA DESC LIMIT 10;`);
-    res.json(response.rows);
+    try {
+        const response = await pool.query(`SELECT authors.name, SUM(VENTAS) AS INGRESO_VENTA FROM
+        (SELECT books.author_id, quantity*item_price AS VENTAS FROM books INNER JOIN sale_items ON books.id = sale_items.book_id)
+        AS T1 INNER JOIN authors ON T1.author_id = authors.id GROUP BY authors.name ORDER BY INGRESO_VENTA DESC LIMIT 10;`);
+        res.json(response.rows);
+    } catch (error) {
+        console.log(error);
+        res.json({error});
+    }
 };
 
 //top "count" autores
 const getTopAuthorsByCount = async (req, res) => {
-    const count = parseInt(req.params.count);
-    if (count>=0) {
+    try {
+        const count = parseInt(req.params.count);
         const response = await pool.query(`SELECT authors.name, SUM(VENTAS) AS INGRESO_VENTA FROM
         (SELECT books.author_id, quantity*item_price AS VENTAS FROM books INNER JOIN sale_items ON books.id = sale_items.book_id)
         AS T1 INNER JOIN authors ON T1.author_id = authors.id GROUP BY authors.name ORDER BY INGRESO_VENTA DESC LIMIT $1;`,[count]);
         res.json(response.rows);
-    } else {
+    } catch (error) {
+        console.log(error);
         res.status(404).json({
             status: 'Error',
             message: "el parametro 'count' no debe ser negativo"
