7e068c142b92f183398e3467bc7b2d4ad68ba985
KevinPC98 <kevin.paredescaman20@gmail.com>
2021-12-08 18:54:43 -0500

    Ligera modificación en el formato JSON

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("port",3000);
-
+app.set("json spaces",2);
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
